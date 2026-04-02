d54148d5acba8f351ab9071baac289b1953ff519
chrysa <greau.anthony@gmail.com>
2026-04-02 23:03:28 +0200

    fix(ci): simplify GitVersion config and remove debug console.log calls

--- src/components/loaders/GlobalLoader.tsx
+++ src/components/loaders/GlobalLoader.tsx
@@ -1,8 +1,6 @@
 import styles from "./GlobalLoader.module.scss";
 
 export default function GlobalLoader() {
-  console.log("GLOBAL LOADER RENDU");
-
   return (
     <div className={styles.overlay}>
       <div className={styles.spinner}></div>
